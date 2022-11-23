fa361958555811dcd3477960a8a34c62d37159c3
Daniel Gottlieb Dollerup <dgot@baselifescience.com>
2022-11-23 23:51:46 +0100

    simplification and support for multiple iterables as source

--- serengeti/stream.py
+++ serengeti/stream.py
@@ -1,24 +1,49 @@
-from multiprocessing import cpu_count
-from typing import Any, Callable, Generator, Iterable, Union
-from concurrent.futures import as_completed, Future, ThreadPoolExecutor
-
-import ray
 import functools
+from concurrent.futures import Executor, as_completed, Future
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
+from typing import Callable, Generator, Iterable, Union
 
 
-THREADS = ThreadPoolExecutor(cpu_count())
+DEFAULT_EXECUTOR = ThreadPoolExecutor(cpu_count())
 
 
-def ray_executor(value, func, *args, **kwargs) -> Future:
-    """Execute function on value using Ray"""
-    _func = ray.remote(func)
-    object_ref = _func.remote(value, *args, **kwargs)
-    return object_ref.future()
+def stream(function: Callable, *iterables, executor: Executor = None, ordered=False) -> Generator:
+    """Returns a generator equivalent to map(fn, *values)
+    but yielding function call results on iterables as they are completed (async).
 
+    Parameters
+    ----------
+    function : Callable
+        A callable that will take as many arguments as there are
+        passed iterables.
+    executor : Executor
+        The executor instance to submit function calls to, will default to
+        a ThreadPoolExecutor if nothing is provided.
+    ordered : bool, optional
+        Yield results in same order as they are passed, by default False.
 
-def thread_executor(value, func, **kwargs) -> Future:
-    """Execute function on value in a thread pool"""
-    return THREADS.submit(func, value, **kwargs)
+    Returns
+    -------
+    Generator
+        An iterator equivalent to: map(func, *iterables) but the calls may
+        be evaluated and yielded out-of-order (async).
+
+    """
+    executor = (
+        executor or DEFAULT_EXECUTOR
+    )
+    futures = (
+        executor.submit(function, *args)
+        for args in zip(*iterables)
+    )
+    if not ordered:
+        futures = as_completed(futures)
+    results = (  # safe unpack of future results
+        future.result() if isinstance(future, Future)
+        else future for future in futures
+    )
+    return results
 
 
 class StreamMonad:
@@ -31,33 +56,29 @@ class StreamMonad:
     using the configured executor. Bind does not apply the function directly,
     but creates a generator of results and wraps it in a StreamMonad. As such
     this new StreamMonad represents the iterable downstream of results, as the
-    function is applied on the current iterable StreamMonad.
+    function is applied on the current iterable source.
 
     """
-
-    def __init__(self, source: Union[Iterable, Generator], executor=thread_executor):
-        self.executor = executor
-        if isinstance(source, (Iterable, Generator)):
-            self.source = source
-        else:
-            raise TypeError("Source must be a Generator or Iterable")
+    def __init__(self, *iterables: Union[Iterable, Generator], executor: Executor = None):
+        for iterable in iterables:
+            if not isinstance(iterable, (Iterable, Generator)):
+                raise TypeError("Source must be a Generator or Iterable")
+        self.iterables = iterables
+        self.executor = executor or DEFAULT_EXECUTOR
 
     def __iter__(self):
-        return self.source
+        return (  # unpack if iterables only contain 1 value
+            values[0] if len(values) == 1 else values
+            for values in zip(*self.iterables)
+        )
 
-    def bind(self, function: Callable, ordered=False, **kwargs) -> "StreamMonad":
+    def bind(self, function: Callable, ordered=False, *args, **kwargs) -> "StreamMonad":
         """Bind function to the current StreamMonad's iterable source
 
         Parameters
         ----------
         function: Callable
             Function to map over the StreamMonad's source
-
-        ordered: bool, default is False
-            When False, function will be applied async and results will be yielded
-            as they are available. Settings this to True will ensure that
-            results of applying function on the stream, are yielded in the same
-            order as the values are received.
 
         Returns
         --------
@@ -66,27 +87,13 @@ class StreamMonad:
             mapped over the current StreamMonad.
 
         """
-        function = functools.partial(function, **kwargs)
-        source = self._stream(function, ordered=ordered)
+        function = functools.partial(function, *args, **kwargs)
+        source = stream(function, self, executor=self.executor, ordered=ordered)
         return StreamMonad(source)
 
     def pipe(self, *functions) -> "StreamMonad":
-        """Pipe the functions on this stream"""
+        """Pipe functions on this stream"""
         return pipe(self.source, *functions)
-
-    def _stream(self, function, executor=ray_executor, ordered=False) -> Generator:
-        """Map function over source async or ordered"""
-        executor = functools.partial(self.executor, func=function)
-        futures = (executor(value) for value in self.source)
-        if not ordered:
-            futures = as_completed(futures)
-        results = (self._get_result(future) for future in futures)
-        return results
-
-    def _get_result(self, result: Union[Future, Any]):
-        if isinstance(result, Future):
-            return result.result()
-        return result
 
 
 def pipe(source: Union[Iterable, Generator], *functions: Callable) -> StreamMonad:
